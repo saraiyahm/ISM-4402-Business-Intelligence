26bb6f476e01cf90195937e4f9f20a9db5ba79e4
saraiyahm <70179566+saraiyahm@users.noreply.github.com>
2020-09-25 23:26:15 -0400

    Update and rename Workbook Assignment 2 - 2.py to Workbook Assignment 3.py

---
+++ Workbook Assignment 3.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+names = ['Bob','Jessica','Mary',John','Mel']
+grades = [76,-2,77,78,101]
+
+GradeList = zip(names,grades)
+df = pd.DataFrame(data = GradeList,
+                  columns=['Names','Grades'])
+df
+
+
+# In[15]:
+
+
+df.loc[df['Grades'] > = 0]
+
+
+# In[18]:
+
+
+df.loc[(df['Grades'] <= 0,'Grades')] = 0
+
+
+# In[ ]:
+
+
+
+
