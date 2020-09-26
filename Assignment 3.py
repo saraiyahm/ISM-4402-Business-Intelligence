89173449704e7fcf6a74a069e5cfe5a51df2a1fe
saraiyahm <70179566+saraiyahm@users.noreply.github.com>
2020-09-25 23:41:59 -0400

    Add files via upload

---
+++ Assignment 3.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+names = ['Bob','Jessica','Mary','John','Mel']
+grades = [76,-2,77,78,101]
+
+
+# In[3]:
+
+
+GradeList = zip(names,grades)
+df = pd.DataFrame(data = GradeList,
+                  columns=['Names','Grades'])
+df
+
+
+# In[10]:
+
+
+df.loc[df['Grades'] <= 0]
+
+
+# In[11]:
+
+
+df.loc[(df['Grades'] >= 0,'Grades')] = 0
+
+
+# In[ ]:
+
+
+
+
